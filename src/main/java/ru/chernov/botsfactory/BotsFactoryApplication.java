768d29b5f7566549c1c448cedfb14a915e879976
Vladimir <vchs1710@yandex.ru>
2021-12-10 01:34:40 +0300

    create repo

---
+++ src/main/java/ru/chernov/botsfactory/BotsFactoryApplication.java
@@ -0,0 +1,13 @@
+package ru.chernov.botsfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotsFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotsFactoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/chernov/botsfactory/BotsFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.chernov.botsfactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotsFactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
